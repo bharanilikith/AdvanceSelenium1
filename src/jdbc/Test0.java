0f6ff17b4e0c11248dceed0e9fb26e996e95bcb7
Bharanilikith <Bharanilikith@Bharani>
2022-12-19 18:05:37 +0530

    API testing

--- src/jdbc/Test0.java
+++ src/jdbc/Test0.java
@@ -5,6 +5,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.mysql.cj.jdbc.Driver;
 
@@ -17,18 +22,31 @@ public class Test0 {
 		DriverManager.registerDriver(dref);
 		
 //Step:2 Connect to database
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo","root","root");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wcse15","root","root");
 		
 //Step:3 Create sql query Statement
 		Statement stat = con.createStatement();
-		String query="select * from project";
+		String query="select * from logindetails";
 		
 //Step:4 execute query
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.actitime.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		
 		ResultSet res = stat.executeQuery(query);
 		while(res.next()) {
+			driver.findElement(By.id("username")).sendKeys(res.getString(1));
+			driver.findElement(By.name("pwd")).sendKeys(res.getString(2));
+					
 			System.out.println(res.getString(1));
+		    System.out.println(res.getString(2));
 		}
-		
+	
+
+
+
 //step:5 close the database
 		con.close();
 		
